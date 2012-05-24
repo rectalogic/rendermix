e453048c823bdf85359e1d0319b846328c6e2fd2
Andrew Wason <rectalogic@rectalogic.com>
2012-05-24 12:32:50 -0400

    More ContextManager specs.

--- spec/context_manager_spec.rb
+++ spec/context_manager_spec.rb
@@ -1,14 +1,8 @@
 require 'spec_helper'
 
-shared_examples 'a context manager' do |render_av_method, av_context_released_method|
+shared_examples 'a context manager' do |render_av_method, av_context_released_method, has_initial_context|
   include_context 'requires render thread'
   let(:renderer) { renderer = double('renderer') }
-
-  def should_render(render_method)
-    renderer.should_receive(render_method) do |mgr|
-      mgr.acquire_context(renderer)
-    end.with(manager)
-  end
 
   it 'should not release the context if the renderer renderered' do
     on_render_thread do
@@ -21,7 +15,7 @@ shared_examples 'a context manager' do |render_av_method, av_context_released_me
     end
   end
 
-  it 'should release the context if nothing rendered' do
+  it 'should release the context if nothing rendered after a render' do
     on_render_thread do
       context = nil
       renderer.should_receive(render_av_method) do |mgr|
@@ -39,28 +33,73 @@ shared_examples 'a context manager' do |render_av_method, av_context_released_me
       manager.render(renderer)
     end
   end
-end
 
-#XXX test cloning
+  it 'should not release the context if nothing ever rendered' do
+    on_render_thread do
+      renderer.should_receive(render_av_method).exactly(3).times
+      renderer.should_not_receive(av_context_released_method)
+
+      manager.render(renderer)
+      manager.render(renderer)
+      manager.render(renderer)
+    end
+  end
+
+  it 'should not reuse the context' do
+    on_render_thread do
+      # Acquire the context so when we render and don't acquire, we'll release
+      context = manager.acquire_context(renderer)
+      renderer.should_receive(render_av_method).once
+      renderer.should_receive(av_context_released_method).once
+      manager.render(renderer)
+
+      # Should still be the same context
+      context.should be manager.acquire_context(renderer)
+    end
+  end
+
+  it 'should not clone the current context' do
+    on_render_thread do
+      context = manager.acquire_context(renderer)
+      manager.clone.acquire_context(renderer).should_not be context
+    end
+  end
+
+  it 'should clone the context pool' do
+    on_render_thread do
+      # Acquire the context so when we render and don't acquire, we'll release
+      context = manager.acquire_context(renderer)
+      renderer.should_receive(render_av_method).once
+      renderer.should_receive(av_context_released_method).once
+      manager.render(renderer)
+
+      # The clone should acquire the pooled context, unless we have an initial context
+      if has_initial_context
+        manager.clone.acquire_context(renderer).should_not be context
+      else
+        manager.clone.acquire_context(renderer).should be context
+      end
+    end
+  end
+end
 
 module RenderMix
   describe AudioContextManager do
-    it_should_behave_like 'a context manager', :render_audio, :audio_context_released do
+    it_should_behave_like 'a context manager', :render_audio, :audio_context_released, false do
       let!(:manager) { AudioContextManager.new(1024) }
     end
 
     context 'with initial context' do
-      it_should_behave_like 'a context manager', :render_audio, :audio_context_released do
+      it_should_behave_like 'a context manager', :render_audio, :audio_context_released, true do
         let!(:manager) do
           AudioContextManager.new(1024, AudioContext.new(1024))
         end
-        #XXX
       end
     end
   end
 
   describe VisualContextManager do
-    it_should_behave_like 'a context manager', :render_visual, :visual_context_released do
+    it_should_behave_like 'a context manager', :render_visual, :visual_context_released, false do
       let!(:manager) do
         on_render_thread do
           VisualContextManager.new(@app.renderManager, @mixer.width,
@@ -71,7 +110,7 @@ module RenderMix
     end
 
     context 'with initial context' do
-      it_should_behave_like 'a context manager', :render_visual, :visual_context_released do
+      it_should_behave_like 'a context manager', :render_visual, :visual_context_released, true do
         let!(:manager) do
           on_render_thread do
             visual_context = VisualContext.new(@app.renderManager, @mixer.tpf,
