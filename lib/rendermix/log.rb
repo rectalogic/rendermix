881d8f5fe151f11a63c90e9102311c766bd64822
Andrew Wason <rectalogic@rectalogic.com>
2012-06-01 18:20:43 -0400

    Update logging.

--- lib/rendermix/log.rb
+++ lib/rendermix/log.rb
@@ -1,23 +1,27 @@
 module RenderMix
   module Log
     LEVEL_MAP = {
-      JavaLogging::Level::OFF => RawMedia::Log::LEVEL_QUIET,
-      JavaLogging::Level::SEVERE => RawMedia::Log::LEVEL_ERROR,
-      JavaLogging::Level::WARNING => RawMedia::Log::LEVEL_WARNING,
-      JavaLogging::Level::INFO => RawMedia::Log::LEVEL_INFO,
-      JavaLogging::Level::CONFIG => RawMedia::Log::LEVEL_VERBOSE,
-      JavaLogging::Level::FINE => RawMedia::Log::LEVEL_VERBOSE,
-      JavaLogging::Level::FINER => RawMedia::Log::LEVEL_DEBUG,
-      JavaLogging::Level::FINEST => RawMedia::Log::LEVEL_DEBUG,
-      JavaLogging::Level::ALL => RawMedia::Log::LEVEL_DEBUG,
+      JavaLog::Level::OFF => RawMedia::Log::LEVEL_QUIET,
+      JavaLog::Level::SEVERE => RawMedia::Log::LEVEL_ERROR,
+      JavaLog::Level::WARNING => RawMedia::Log::LEVEL_WARNING,
+      JavaLog::Level::INFO => RawMedia::Log::LEVEL_INFO,
+      JavaLog::Level::CONFIG => RawMedia::Log::LEVEL_VERBOSE,
+      JavaLog::Level::FINE => RawMedia::Log::LEVEL_VERBOSE,
+      JavaLog::Level::FINER => RawMedia::Log::LEVEL_DEBUG,
+      JavaLog::Level::FINEST => RawMedia::Log::LEVEL_DEBUG,
+      JavaLog::Level::ALL => RawMedia::Log::LEVEL_DEBUG,
       nil => RawMedia::Log::LEVEL_INFO,
     }
+
     # Configure RawMedia logging
-    logger = java.util.logging.Logger.getLogger('RawMedia')
-    until logger.nil? or (level = logger.level)
-      logger = logger.parent
+    logger = JavaLog::Logger.getLogger('RawMedia')
+    current = logger
+    until current.nil? or (level = current.level)
+      current = current.parent
     end
-    RawMedia::Log.set_callback(LEVEL_MAP[level],
-                               Proc.new {|msg| logger.info(msg) })
+    log = Proc.new do |msg|
+      logger.logp(level, 'RawMedia', 'log', msg)
+    end
+    RawMedia::Log.set_callback(LEVEL_MAP[level], log)
   end
 end
